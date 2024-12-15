3043097fb8fdff378410919459399432c3a0f097
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-12-15 11:28:31 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int A = 15;
+    int C = 19;
+    int B = (A + C)/2;
+    printf("%d\n",B);
+    A=16;
+    C=18;
+    B=(A+C)/2;
+    printf("%d\n",B);
+    return 0;
+}
